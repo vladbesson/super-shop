18f1a4dc27298a63954a886e40a037bcf5c5cc9b
Vladislav Balabanovich <vladislav.svoi@gmail.com>
2022-06-12 17:38:18 +0200

    small refactoring

--- js/data.js
+++ js/data.js
@@ -3,60 +3,54 @@ export const products = [
     name: "Товар 1",
     text: "Товар 1. Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old",
     cost: 100,
+    cover: 'https://i.postimg.cc/T2jxDq28/1.jpg'
   },
   {
     name: "Товар 2",
     text: "Товар 2. Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old",
     cost: 200,
+    cover: 'https://i.postimg.cc/7hwpVdHF/2.jpg'
   },
   {
     name: "Товар 3",
     text: "Товар 3. Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old",
     cost: 300,
+    cover: 'https://i.postimg.cc/3JnHL8Dn/3.jpg'
   },
   {
     name: "Товар 4",
     text: "Товар 4. Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old",
     cost: 400,
+    cover: 'https://i.postimg.cc/xCDrkqWT/4.jpg'
   },
   {
     name: "Товар 5",
     text: "Товар 5. Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old",
     cost: 500,
+    cover: 'https://i.postimg.cc/0QpT7PsG/5.jpg'
   },
   {
     name: "Товар 6",
     text: "Товар 6. Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old",
     cost: 600,
+    cover: 'https://i.postimg.cc/pLSMHsCj/6.jpg'
   },
   {
     name: "Товар 7",
     text: "Товар 7. Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old",
     cost: 125,
+    cover: 'https://i.postimg.cc/x18VhwvY/7.jpg'
   },
   {
     name: "Товар 8",
     text: "Товар 8. Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old",
     cost: 225,
+    cover: 'https://i.postimg.cc/VNbQLXZV/8.jpg'
   },
   {
     name: "Товар 9",
     text: "Товар 9. Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old",
     cost: 325,
-  },
-  {
-    name: "Товар 10",
-    text: "Товар 10. Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old",
-    cost: 425,
-  },
-  {
-    name: "Товар 11",
-    text: "Товар 11. Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old",
-    cost: 525,
-  },
-  {
-    name: "Товар 12",
-    text: "Товар 12. Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old",
-    cost: 625,
-  },
+    cover: 'https://i.postimg.cc/159Qcn48/9.jpg'
+  }
 ];
